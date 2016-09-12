c7d6257dc21723eb5d26f0dd0f598445da9475cf
Josh Burks <dotnofoolin@gmail.com>
2016-09-12 10:54:52 -0500

    Disable multi expand.

--- static/site.js
+++ static/site.js
@@ -2,7 +2,7 @@ $(document).ready(function () {
     // Configure the foundation features
     $(document).foundation({
         accordion: {
-            multi_expand: true,
+            multi_expand: false,
         }
     });
 
