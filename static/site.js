a487be181b4b3f0fc8a462628c25e0340122c2eb
Josh Burks <dotnofoolin@gmail.com>
2016-09-14 16:11:49 -0500

    Update index to use layout template. Make Foundation classes play nice now that it's version 6.

--- static/site.js
+++ static/site.js
@@ -1,9 +1,3 @@
 $(document).ready(function () {
-    // Configure the foundation features
-    $(document).foundation({
-        accordion: {
-            multi_expand: false,
-        }
-    });
-
+    $(document).foundation();
 });
